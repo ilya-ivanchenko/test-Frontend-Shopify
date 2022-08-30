eb119c3efbb6e32df1dd7727cc1d69f3fb456a11
ivanchenko <90793566+ivanchenko92@users.noreply.github.com>
2022-08-30 10:19:43 +0300

    Add files via upload

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -4,27 +4,52 @@ const buttonVisible = document.querySelector(".collapsible__action--visible");
 const buttonHidden = document.querySelector(".collapsible__action--hidden");
 
 button.addEventListener("click", workWithContent);
-document.addEventListener("DOMContentLoaded", hideContent);
+
+document.addEventListener("DOMContentLoaded", firstHideContent);
+
+let isVisible = false;
 
 function workWithContent() {
-    
-    if (content.hidden) {
-        showContent();
-    }
-    else {
-        hideContent();  
-    }
+    isVisible ? hideContent() : showContent();
 }
 
-function hideContent() {
-    content.hidden = true;
+function firstHideContent() {
+    buttonVisible.hidden = true;
+    buttonHidden.hidden = false;
+    content.style.opacity = 0;
+}
+
+function animateContent(direction) {
+    const animateParams = [
+        {transform: 'translate(0, 5px)', opacity: 1 },
+        {transform: 'translate(0, -10px)', opacity: 0, offset: .8},
+        {transform: 'translate(0, -20px)', opacity: 0}
+    ];
+    const animateSettings = {  
+        duration: 800,
+        iterations: 1,
+        easing: 'ease-in-out',
+        fill: 'forwards'
+    }
+    direction ? animateSettings.direction = direction : null;
+    
+    content.animate(animateParams, animateSettings);
+}
+
+function hideContent() {     
+    animateContent();
+    isVisible = false;
     buttonVisible.hidden = true;
     buttonHidden.hidden = false;
 } 
 
 function showContent() {
-    content.hidden = false;
+    animateContent('reverse');  
+    isVisible = true;
     buttonVisible.hidden = false;
-    buttonHidden.hidden = true;
+    buttonHidden.hidden = true;  
 }
 
+//добавим красоты
+
+button.style.width = '155px';
