7a83ff33e8f47f7632c2898352acfcf09162ec76
ilya.ivanchenko <90793566+ivanchenko92@users.noreply.github.com>
2022-08-31 22:13:52 +0300

    for checking

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector(".collapsible__button");
-const content = document.querySelector(".collapsible__content"); 
-const buttonVisible = document.querySelector(".collapsible__action--visible"); 
+const content = document.querySelector(".collapsible__content");
+const buttonVisible = document.querySelector(".collapsible__action--visible");
 const buttonHidden = document.querySelector(".collapsible__action--hidden");
 
 button.addEventListener("click", workWithContent);
@@ -16,40 +16,41 @@ function workWithContent() {
 function firstHideContent() {
     buttonVisible.hidden = true;
     buttonHidden.hidden = false;
-    content.style.opacity = 0;
+    content.style.opacity = '0';
 }
 
 function animateContent(direction) {
     const animateParams = [
-        {transform: 'translate(0, 5px)', opacity: 1 },
+        {transform: 'translate(0, 5px)', opacity: 1, offset: 0},
         {transform: 'translate(0, -10px)', opacity: 0, offset: .8},
-        {transform: 'translate(0, -20px)', opacity: 0}
+        {transform: 'translate(0, -20px)', opacity: 0, offset: 1}
     ];
-    const animateSettings = {  
-        duration: 800,
+    const animateSettings = {
+        duration: 700,
         iterations: 1,
         easing: 'ease-in-out',
         fill: 'forwards'
     }
     direction ? animateSettings.direction = direction : null;
-    
+
     content.animate(animateParams, animateSettings);
 }
 
-function hideContent() {     
+function hideContent() {
     animateContent();
     isVisible = false;
     buttonVisible.hidden = true;
     buttonHidden.hidden = false;
-} 
-
-function showContent() {
-    animateContent('reverse');  
-    isVisible = true;
-    buttonVisible.hidden = false;
-    buttonHidden.hidden = true;  
 }
 
-//добавим красоты
+function showContent() {
+    animateContent('reverse');
+    isVisible = true;
+    buttonVisible.hidden = false;
+    buttonHidden.hidden = true;
+}
+
+//добавим немного красоты
 
 button.style.width = '155px';
+button.style.cursor = 'pointer';
